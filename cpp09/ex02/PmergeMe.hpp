386973be699d3e1bb1dc3b95abbd38a2a148da60
GoogelTrad <celianmichez@free.fr>
2024-03-13 03:16:20 +0100

    ex02 start

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmichez <cmichez@student-42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 21:47:01 by cmichez           #+#    #+#             */
+/*   Updated: 2024/03/12 21:52:30 by cmichez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+class PmergeMe
+{
+private:
+	std::vector<int> stack;
+public:
+	PmergeMe();
+	~PmergeMe();
+};
